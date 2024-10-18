ada40c2d8223427ae7331fce156847bb69fe5df4
Viral Patel <pviral3011@gmail.com>
2024-10-18 14:09:22 +0530

    service dependency injection

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { NewTaskComponent } from './new-task/new-task.component';
+import { TasksListComponent } from './tasks-list/tasks-list.component';
+// import { TasksService } from './tasks.service';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  templateUrl: './tasks.component.html',
+  imports: [NewTaskComponent, TasksListComponent],
+  // Element Injector 
+  // Operate with different instance on duplicate component 
+  // providers:[TasksService]
+})
+export class TasksComponent {}
